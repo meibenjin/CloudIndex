48d9fc28c8d8422b7ddb9b11c145f84e9d78e4d4
meibenjin <meibenjin@gmail.com>
2013-09-20 00:25:21 +0800

    update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -6,16 +6,37 @@ upload_dir=/home/iir/mbj
 remote_shell="rm ${upload_dir}/* -rf"
 host_prefix="iir@10.77.30"
 
-for i in 101 199 200 201 202 203 204 205
-do 
-    remote_host="${host_prefix}.$i"
-    ssh ${remote_host} "${remote_shell}"
-    echo "clean up directory ${remote_host}:${upload_dir}"
-done
+function clean_remote_dir()
+{
+    for i in 101 199 200 201 202 203 204 205
+    do 
+        remote_host="${host_prefix}.$i"
+        ssh ${remote_host} "${remote_shell}"
+        echo "clean up directory ${remote_host}:${upload_dir}"
+    done
+}
 
-for i in 101 199 200 201 202 203 204 205
-do 
-    remote_host="${host_prefix}.$i"
-    scp -r -q ${local_dir} ${remote_host}:${upload_dir}
-    echo "copy to ${remote_host}"
-done
+function copy_it()
+{
+    for i in 101 199 200 201 202 203 204 205
+    do 
+        remote_host="${host_prefix}.$i"
+        scp -r -q ${local_dir} ${remote_host}:${upload_dir}
+        echo "copy to ${remote_host}"
+    done
+}
+
+if [ $# = 0 ]; then
+    $(clean_remote_dir)
+    $(copy_it)
+fi
+
+COMMAND=$1
+
+if [ "$COMMAND" = "clean" ]; then
+    $(clean_remote_dir)
+elif [ "$COMMAND" = "copy" ]; then
+    $(copy_it)
+fi
+
+
