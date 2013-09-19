e12c8ab24820f7b93e288eb5842efb5d4a9df60f
meibenjin <meibenjin@gmail.com>
2013-09-20 02:51:29 +0800

    update utils.h

--- utils.h
+++ utils.h
@@ -1,23 +1,39 @@
 /*
- * head.h
+ * utils.h
  *
  *  Created on: Sep 16, 2013
  *      Author: meibenjin
  */
 
-#ifndef HEAD_H_
-#define HEAD_H_
+#ifndef UTILS_H_
+#define UTILS_H_
 
-#define MAX_IP_ADDR_LENGTH 20
+// limits for torus
+#define MAX_NEIGHBORS 6
+#define MAX_NODES_NUM 10 * 10 * 10
 
-enum {
+
+// 3-dimension coordinate
+typedef struct coordinate
+{
+	int x;
+	int y;
+	int z;
+}coordinate;
+
+// return status of functions
+enum status
+{
 	SUCESS = 0, FAILED = -1
 };
 
-enum OP {
+//operations for torus nodes in communication 
+enum OP
+{
 	HELLO = 80, READ, WRITE,
 };
 
+#endif /* UTILS_H_ */
 
 
-#endif /* HEAD_H_ */
+
