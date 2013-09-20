220333d650a1981d687a3f6cd7d0c2f8c092a336
meibenjin <meibenjin@gmail.com>
2013-09-21 04:07:40 +0800

    update start_server_test

--- test/start_server_test.c
+++ test/start_server_test.c
@@ -7,10 +7,14 @@
 
 #include<stdio.h>
 #include"../socket/socket_server.h"
+#include"../torus_node.h"
 
 int main(int argc, char **argv)
 {
 	int server_socket;
+
+    init_torus_node(local_torus_node);
+
     server_socket = start_server_socket();
 	return 0;
 }
