7799391af650f27f0da100dc19e56db453d122da
meibenjin <meibenjin@gmail.com>
2013-09-21 04:06:56 +0800

    update utils.h

--- utils.h
+++ utils.h
@@ -8,6 +8,25 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+
+// limits for socket
+#define MAX_IP_ADDR_LENGTH 20
+#define LISTEN_PORT 10086
+#define LISTEN_QUEUE_LENGTH 20
+#define SOCKET_BUF_SIZE 1024 
+#define MAX_REPLY_SIZE sizeof(int) 
+#define MAX_DATA_SIZE 1000   
+#define SOCKET_ERROR -1
+
+// message for communication among torus nodes
+typedef struct message
+{
+	int op;
+	char src_ip[MAX_IP_ADDR_LENGTH];
+	char dst_ip[MAX_IP_ADDR_LENGTH];
+    char data[MAX_DATA_SIZE];
+}message;
+
 // limits for torus
 #define MAX_NEIGHBORS 6
 #define MAX_NODES_NUM 10 * 10 * 10
@@ -24,13 +43,26 @@ typedef struct coordinate
 // return status of functions
 enum status
 {
-	SUCESS = 0, FAILED = -1
+	TRUE = 0, FALSE = -1
 };
+
+// reply for the client request
+enum reply
+{
+    SUCCESS = 1,
+    FAILED,
+    // incorrect operations 
+    WRONG_OP
+};
+    
 
 //operations for torus nodes in communication 
 enum OP
 {
-	HELLO = 80, READ, WRITE,
+    // update torus node info
+    UPDATE_TORUS = 80,
+    READ, 
+    WRITE,
 };
 
 #endif /* UTILS_H_ */
