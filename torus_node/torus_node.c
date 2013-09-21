3be052bcbc5e2a9b473e1b4a5c3f88f72f03fc13
meibenjin <meibenjin@gmail.com>
2013-09-21 16:04:46 +0800

    update torus_node/torus_node.* files

--- torus_node/torus_node.c
+++ torus_node/torus_node.c
@@ -13,7 +13,22 @@
 
 void set_node_ip(torus_node *node_ptr, const char *ip)
 {
-	strncpy(node_ptr->node_ip, ip, MAX_IP_ADDR_LENGTH);
+	if (!node_ptr)
+    {
+		printf("set_node_ip: node_ptr is null pointer\n");
+		return;
+	}
+	strncpy(node_ptr->info.ip, ip, MAX_IP_ADDR_LENGTH);
+}
+
+void get_node_ip(torus_node node, char *ip)
+{
+    if( ip == NULL)
+    {
+        printf("get_node_ip: ip is null pointer\n");
+        return;
+    }
+    strncpy(node.info.ip, ip, MAX_IP_ADDR_LENGTH);
 }
 
 void set_coordinate(torus_node *node_ptr, int x, int y, int z)
@@ -23,14 +38,30 @@ void set_coordinate(torus_node *node_ptr, int x, int y, int z)
 		printf("set_coordinate: node_ptr is null pointer\n");
 		return;
 	}
-	node_ptr->node_id.x = x;
-	node_ptr->node_id.y = y;
-	node_ptr->node_id.z = z;
+	node_ptr->info.id.x = x;
+	node_ptr->info.id.y = y;
+	node_ptr->info.id.z = z;
+}
+
+void set_neighbors_num(torus_node *node_ptr, int neighbors_num)
+{
+	if (!node_ptr)
+    {
+		printf("set_neighbors_num: node_ptr is null pointer\n");
+		return;
+	}
+
+    node_ptr->info.neighbors_num = neighbors_num;
+}
+
+int get_neighbors_num(torus_node node)
+{
+    return node.info.neighbors_num;
 }
 
 void print_coordinate(torus_node node)
 {
-	printf("(%d, %d, %d)\n", node.node_id.x, node.node_id.y, node.node_id.z);
+	printf("(%d, %d, %d)\n", node.info.id.x, node.info.id.y, node.info.id.z);
 }
 
 void init_torus_node(torus_node *node_ptr)
@@ -39,7 +70,8 @@ void init_torus_node(torus_node *node_ptr)
 	set_node_ip(node_ptr, "0.0.0.0");
 
 	set_coordinate(node_ptr, -1, -1, -1);
-    node_ptr->neighbors_num = 0;
+
+    set_neighbors_num(node_ptr, 0);
 
 	for (i = 0; i < MAX_NEIGHBORS; ++i)
     {
--- torus_node/torus_node.h
+++ torus_node/torus_node.h
@@ -15,25 +15,22 @@ typedef struct node_info
 {
 	// torus node ip address
 	char ip[MAX_IP_ADDR_LENGTH];
+
+	// torus node coordinate
     struct coordinate id;
+
+    // number of neighbors
+    int neighbors_num;
 
 }node_info;
 
 typedef struct torus_node
 {
-	// torus node ip address
-	char node_ip[MAX_IP_ADDR_LENGTH];
-
-	// torus node coordinate
-	struct coordinate node_id;
-
-    // number of neighbors
-    int neighbors_num;
+    // torus node info
+    struct node_info info;
 
 	// torus node's neighbors 
 	struct torus_node *neighbors[MAX_NEIGHBORS];
-
-
 }torus_node;
 
 struct torus_node local_torus_node;
@@ -41,8 +38,15 @@ struct torus_node local_torus_node;
 // set torus node ip
 void set_node_ip(torus_node *node_ptr, const char *ip);
 
+void get_node_ip(torus_node node, char *ip);
+
 // set the coordinate of a torus node
 void set_coordinate(torus_node *node_ptr, int x, int y, int z);
+
+// set the neighbors num of a torus node
+void set_neighbors_num(torus_node *node_ptr, int neighbors_num);
+
+int get_neighbors_num(torus_node node);
 
 void print_coordinate(torus_node node);
 
