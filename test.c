88f756de9d32e99e82b4b939591a97b39eacdbb5
meibenjin <meibenjin@gmail.com>
2013-10-19 00:34:37 +0800

    update file test.c

--- test.c
+++ test.c
@@ -9,9 +9,21 @@
 #include<stdlib.h>
 #include<string.h>
 
+void show_bytes(char *p, int size) {
+    int i;
+    for(i = 0; i < size; i++){
+        printf("0x%.2x ", *p++);
+    }
+    printf("\n");
+}
+
 int main(void) {
-	char test[40];
-	memset(test, 0, 40);
-	printf("%d\n", strcmp(test, ""));
+    int a = 0x01;
+    int l = htonl(a);
+    printf("%d:", a);
+    show_bytes((void *)&a, sizeof(int));
+    printf("%d:", l);
+    show_bytes((void *)&l, sizeof(int));
+    return 0;
 }
 
