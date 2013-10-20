2f9592bb0bc90ede6b961ff86417dc062b96a332
meibenjin <meibenjin@gmail.com>
2013-10-20 21:48:46 +0800

    move definitions to utils.h

--- utils.h
+++ utils.h
@@ -19,6 +19,14 @@
 #define SOCKET_ERROR -1
 #define STAMP_SIZE 40 
 
+
+// limits for torus
+#define MAX_NEIGHBORS 6
+#define MAX_NODES_NUM 20 * 20 * 20
+
+// limits for skip list
+#define MAXLEVEL 31
+
 // 3-dimension coordinate
 typedef struct coordinate {
 	int x;
@@ -43,10 +51,53 @@ typedef enum OP {
 	// update torus node info
 	UPDATE_TORUS = 80,
 	UPDATE_SKIP_LIST,
-	TRAVERSE,
+	TRAVERSE_TORUS,
+    TRAVERSE_SKIP_LIST,
 	READ,
 	WRITE,
 } OP;
+
+// torus node info 
+typedef struct node_info {
+	// torus node ip address
+	char ip[IP_ADDR_LENGTH];
+
+	// cluster id the node belongs to
+	int cluster_id;
+
+	// torus node coordinate
+	struct coordinate node_id;
+
+} node_info;
+
+typedef struct torus_node {
+	// torus node info
+	struct node_info info;
+
+	// number of neighbors
+	int neighbors_num;
+
+	// torus node's neighbors
+	struct torus_node *neighbors[MAX_NEIGHBORS];
+} torus_node;
+
+
+//skip list node structure
+typedef struct skip_list_node {
+	node_info leader;
+    int height;
+    struct skip_list_level{
+        struct skip_list_node *forward;
+        struct skip_list_node *backward;
+    }level[];
+}skip_list_node;
+
+// header of skip_list
+typedef struct skip_list{
+	skip_list_node *header;
+	int level;
+}skip_list;
+
 
 #endif /* UTILS_H_ */
 
