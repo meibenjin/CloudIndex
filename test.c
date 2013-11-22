1aef3b57dde6f2cd4382fde477c83c23b762b4e3
meibenjin <meibenjin@gmail.com>
2013-11-22 17:08:55 +0800

    update test.c

--- test.c
+++ test.c
@@ -9,29 +9,23 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct A{
-    int a;
-    char b;
-};
+#include"utils.h"
 
-struct B{
-	int a;
-	struct A n[];
-};
-struct B *new_B(){
-    struct A t = { 100, 'c'};
-    struct B *p;
-    p = (struct B *) malloc(sizeof(struct B) + sizeof(struct A));
-    p->a = 1;
-    //p->n = (struct A *) malloc(sizeof(struct A));
-    memcpy(&p->n[0], (void *)&t, sizeof(struct A));
-    return p;
+int overlaps(interval c[], interval o[]) {
+    int i, ovlp = 1;
+    i = -1;
+    do {
+        i++;
+        ovlp = !(c[i].high < o[i].low || c[i].low > o[i].high); 
+    }while(ovlp && i != MAX_DIM_NUM);
+
+    return ovlp;
 }
+
 int main(void) {
-    struct B *p;
-    p = new_B();
-    printf("%d, %d, %c\n", p->a, p->n->a, p->n->b);
-    free(p);
+    interval c[MAX_DIM_NUM] = {{40, 80}, { 100, 120 }, {100, 120}};
+    interval o[MAX_DIM_NUM] = {{74, 81}, { 113, 118 }, {96, 105}};
+    printf("%d\n", overlaps(c, o));
     return 0;
 }
 
