f21eeee5bd2accfa69eef745846be44dcb59263d
meibenjin <meibenjin@gmail.com>
2013-12-11 20:09:38 +0800

    add rtree_query function

--- torus_node/torus_rtree.c
+++ torus_node/torus_rtree.c
@@ -5,29 +5,180 @@
  *      Author: meibenjin
  */
 
-#include <cstring>
 #include "torus_rtree.h"
 
 #define INSERT 1
 #define DELETE 0
 #define QUERY 2
 
-
+// example of a Visitor pattern.
+// findes the index and leaf IO for answering the query and prints
+// the resulting data IDs to stdout.
 class MyVisitor : public IVisitor
 {
 public:
-	void visitNode(const INode& n) {}
+	size_t m_indexIO;
+	size_t m_leafIO;
+
+public:
+	MyVisitor() : m_indexIO(0), m_leafIO(0) {}
+
+	void visitNode(const INode& n)
+	{
+		if (n.isLeaf()) m_leafIO++;
+		else m_indexIO++;
+	}
 
 	void visitData(const IData& d)
 	{
-		std::cout << d.getIdentifier() << std::endl;
+		IShape* pS;
+		d.getShape(&pS);
+        // do something.
+		delete pS;
+
+		// data should be an array of characters representing a Region as a string.
+		byte* pData = 0;
+		uint32_t cLen = 0;
+		d.getData(cLen, &pData);
+		// do something.
+		//string s = reinterpret_cast<char*>(pData);
+		//cout << s << endl;
+		delete[] pData;
+
+		cout << d.getIdentifier() << endl;
         // the ID of this data entry is an answer to the query. I will just print it to stdout.
 	}
 
-	void visitData(std::vector<const IData*>& v) {}
+	void visitData(std::vector<const IData*>& v)
+	{
+		cout << v[0]->getIdentifier() << " " << v[1]->getIdentifier() << endl;
+	}
 };
 
-ISpatialIndex* load_rtree()
+// example of a Strategy pattern.
+// traverses the tree by level.
+class MyQueryStrategy : public SpatialIndex::IQueryStrategy
+{
+private:
+	queue<id_type> ids;
+
+public:
+	void getNextEntry(const IEntry& entry, id_type& nextEntry, bool& hasNext)
+	{
+		IShape* ps;
+		entry.getShape(&ps);
+		Region* pr = dynamic_cast<Region*>(ps);
+
+		cout << pr->m_pLow[0] << " " << pr->m_pLow[1] << endl;
+		cout << pr->m_pHigh[0] << " " << pr->m_pLow[1] << endl;
+		cout << pr->m_pHigh[0] << " " << pr->m_pHigh[1] << endl;
+		cout << pr->m_pLow[0] << " " << pr->m_pHigh[1] << endl;
+		cout << pr->m_pLow[0] << " " << pr->m_pLow[1] << endl << endl << endl;
+			// print node MBRs gnuplot style!
+
+		delete ps;
+
+		const INode* n = dynamic_cast<const INode*>(&entry);
+
+		// traverse only index nodes at levels 2 and higher.
+		if (n != 0 && n->getLevel() > 1)
+		{
+			for (uint32_t cChild = 0; cChild < n->getChildrenCount(); cChild++)
+			{
+				ids.push(n->getChildIdentifier(cChild));
+			}
+		}
+
+		if (! ids.empty())
+		{
+			nextEntry = ids.front(); ids.pop();
+			hasNext = true;
+		}
+		else
+		{
+			hasNext = false;
+		}
+	}
+};
+
+// example of a Strategy pattern.
+// find the total indexed space managed by the index (the MBR of the root).
+class MyQueryStrategy2 : public IQueryStrategy
+{
+public:
+	Region m_indexedSpace;
+
+public:
+	void getNextEntry(const IEntry& entry, id_type& nextEntry, bool& hasNext)
+	{
+		// the first time we are called, entry points to the root.
+
+		// stop after the root.
+		hasNext = false;
+
+		IShape* ps;
+		entry.getShape(&ps);
+		ps->getMBR(m_indexedSpace);
+		delete ps;
+	}
+};
+
+int rtree_query(int op, int id, double plow[], double phigh[], int d, ISpatialIndex *rtree) {
+    try
+    {
+		size_t indexIO = 0;
+		size_t leafIO = 0;
+        uint32_t queryType = 0;
+
+        if (op == QUERY)
+        {
+            MyVisitor vis;
+
+            if (queryType == 0)
+            {
+                Region r = Region(plow, phigh, d);
+                rtree->intersectsWithQuery(r, vis);
+                // this will find all data that intersect with the query range.
+            }
+            else if (queryType == 1)
+            {
+                Point p = Point(plow, d);
+                rtree->nearestNeighborQuery(10, p, vis);
+                // this will find the 10 nearest neighbors.
+            }
+            else
+            {
+                Region r = Region(plow, phigh, d);
+                rtree->selfJoinQuery(r, vis);
+            }
+
+            indexIO += vis.m_indexIO;
+            leafIO += vis.m_leafIO;
+                // example of the Visitor pattern usage, for calculating how many nodes
+                // were visited.
+        }
+        else
+        {
+            std::cerr << "This is not a query operation." << endl;
+        }
+    }
+	catch (Tools::Exception& e)
+	{
+		cerr << "******ERROR******" << endl;
+		std::string s = e.what();
+		cerr << s << endl;
+		return -1;
+	}
+	catch (...)
+	{
+		cerr << "******ERROR******" << endl;
+		cerr << "other exception" << endl;
+		return -1;
+	}
+    return 0;
+}
+
+ISpatialIndex* rtree_load()
 {
     ISpatialIndex *tree;
 	try
--- torus_node/torus_rtree.h
+++ torus_node/torus_rtree.h
@@ -8,11 +8,16 @@
 #ifndef TORUS_RTREE_H_
 #define TORUS_RTREE_H_
 
+
+#include<cstring>
 // include library header file.
 #include "spatialindex/SpatialIndex.h"
 
 using namespace SpatialIndex;
+using namespace std;
 
-ISpatialIndex* load_rtree();
+int rtree_query(int op, int id, double plow[], double phigh[], int d, ISpatialIndex *rtree);
+
+ISpatialIndex* rtree_load();
 
 #endif /* TORUS_RTREE_H_ */
