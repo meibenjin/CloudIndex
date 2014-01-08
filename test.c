3194c654458ac895debbd88068dfa39f0451e27f
meibenjin <meibenjin@gmail.com>
2014-01-08 22:03:55 +0800

    update test.c

--- test.c
+++ test.c
@@ -16,27 +16,29 @@ void gen_range(int min, int max, float p) {
     step = max * p;
     max = max - max * p;
 
+    printf("%d %d ", 2, 1);
+    int rlow[3], rhigh[3];
 	for (i = 0; i < 3; i++) {
 		randint = rand();
 		low = (randint % (max - min)) + min;
 		high = low + step - 1 ;
-		printf("%d %d ", low, high);
+        rlow[i] = low;
+        rhigh[i] = high;
 	}
+	for (i = 0; i < 3; i++)
+        printf("%d ", rlow[i]);
+	for (i = 0; i < 3; i++) 
+        printf("%d ", rhigh[i]);
 	printf("\n");
 }
 
-typedef enum test{
-    SUCCESS,
-    FAILED
-}test;
 
 int main(void) {
 	int i;
     srand(time(NULL));
 	for(i = 0; i< 100;i++){
-		gen_range(1, 100, 0.6);
+		gen_range(1, 100, 0.4);
 	}
-
 	return 0;
 }
 
