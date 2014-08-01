af80d785f007883826c5b4bb421b91e4f2c8a64c
meibenjin <meibenjin@gmail.com>
2014-08-01 23:36:43 +0800

    new utils.c:define global properties

---
+++ utils.c
@@ -0,0 +1,21 @@
+/*
+ * utils.c
+ *
+ *  Created on: Aug 1, 2014
+ *      Author: meibenjin
+ */
+
+#include "utils.h"
+
+uint32_t DEFAULT_CAPACITY;
+int HEARTBEAT_INTERVAL;
+int MAX_ROUTE_STEP;
+double SIGMA; 
+int SAMPLE_TIME_POINTS;
+int SAMPLE_SPATIAL_POINTS;
+double SAMPLE_TIME_RATE;
+int MAX_RESPONSE_TIME;
+double EXCHANGE_RATE_RANGE_QUERY;
+double EXCHANGE_RATE_NN_QUERY;
+int ACTIVE_LEADER_NUM;
+int FIXED_IDLE_NODE_NUM;
